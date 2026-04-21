a4630ac64660df365e46f9f68a1d62dc62a4b9d2
qwehgci-creator <276070954+qwehgci-creator@users.noreply.github.com>
2026-04-21 22:27:55 +0500

    прикольная игра (угадай цифру)

--- zadanie-AI.py
+++ zadanie-AI.py
@@ -11,11 +11,11 @@
 #print (f'{a} исполниться будет {b+1} {c} в следующим году')
 
 #2 условные переменые if/else
-a=int(input ('введи число: '))
-if a % 2:
-    print('не чётное')
-else:
-    print ('чётное')
+#a=int(input ('введи число: '))
+#if a % 2:
+#    print('не чётное')
+#else:
+#    print ('чётное')
 
 #3 Цикл for
 #for i in range(1,10+1):
@@ -23,3 +23,17 @@ else:
 #    c=a*i
 #    print(a, '*', i, '=', c)
 
+#4 цикл while
+#import random
+#secret=random.random()*100//10 #само загадное число до 100
+#number=0
+#while secret !=number:
+#    number= int(input('введи чесло, чтобы угадать(целые): '))
+#    print ('не угадал, но число')
+#    if number<secret:
+#        print('малое')
+#    elif number>secret:
+#        print('большое')
+#print('а хотя нет, угадал')
+
+#5 
