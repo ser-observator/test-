f9e4e64d28e7e66ec6ebd7513500782567621032
qwehgci-creator <276070954+qwehgci-creator@users.noreply.github.com>
2026-04-21 20:59:13 +0500

    переменые

--- zadanie-AI.py
+++ zadanie-AI.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env pnthon3
-for i in range(1,10+1):
-    a=7
-    c=a*i
-    print(a, '*', i, '=', c)
+#1 Работа с пременой
+a=str(input("ваше имя: "))
+b=int(input('ваш возраст: '))
+if b<=1:
+    c=str('год')
+elif b<=4:
+    с= str('года')
+else:
+    c= str("лет")
+print (f'{a} исполниться будет {b+1} {c} в следующим году')
+
+#3 Цикл for
+#for i in range(1,10+1):
+#    a=7
+#    c=a*i
+#    print(a, '*', i, '=', c)
+
