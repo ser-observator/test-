75060d6d8a00133722846ffb4ef7b84ca54ca7f4
qwehgci-creator <276070954+qwehgci-creator@users.noreply.github.com>
2026-04-27 16:16:58 +0500

    сука

--- zadachaotAI2.py
+++ zadachaotAI2.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env pnthon3
+
+# Задание 1: Работа со строками
 #name=input('имя= ')
 #print (f'Прив,{name}!')
- 
+
+ # Задание 2: Простые арифметические операции
+otvet=[input("пример: ")]
+otvet= otvet.index
+a= otvet [0]
+b= otvet [2]
+c= otvet [1]
+print (a, c, b)
