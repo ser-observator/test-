de37798bdd5329517efab7bc96fc7b73c77b0eef
qwehgci-creator <276070954+qwehgci-creator@users.noreply.github.com>
2026-05-20 00:20:22 +0500

    бляяя, ебал в рот

--- yandex.py
+++ yandex.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env pnthon3
-a=int(input())
-b=int(input())
-c=int(input())
-if a<b+c and b<c+a and c<a+b:
-    print ('YES')
-else: print ('no')
+#a=int(input())
+#b=int(input())
+#c=int(input())
+#if a<b+c and b<c+a and c<a+b:
+#    print ('YES')
+#else: print ('no')
+stroka, stolbec, mina = map(int, input().split())
+matrix = [[0] *stolbec for _ in range(stroka)]
+
+
+
+
+
+#for row in matrix:
+#    print (' '.join(map(str, row)))
--- zadachaotAI2.py
+++ zadachaotAI2.py
@@ -5,10 +5,14 @@
 #print (f'Прив,{name}!')
 
  # Задание 2: Простые арифметические операции
-primer=input('пример=')
-try:
-    print(eval(primer))
-except ZeroDivisionError:
-    print ('невозможно')
-except:
-    print('невернный ввод')
+#primer=input('пример=')
+#try:
+#    print(eval(primer))
+#except ZeroDivisionError:
+#    print ('невозможно')
+#except:
+#    print('невернный ввод')
+#Задание 3: Условные операторы
+number=int(input('число= '))
+if number>1:
+    print ('')
