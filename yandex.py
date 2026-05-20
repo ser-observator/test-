7ae4bdd29fa744a5b168fbbe667fed1690931c92
qwehgci-creator <276070954+qwehgci-creator@users.noreply.github.com>
2026-05-20 14:10:39 +0500

    lool

--- yandex.py
+++ yandex.py
@@ -10,14 +10,21 @@ stroka, stolbec, mina = map(int, input().split())
 matrix = [[0] *stolbec for _ in range(stroka)]
 
 for _ in range(mina):
-    r,c= map(int, input().split())
+    r, c= map(int, input().split())
     matrix[r-1][c-1]='*'
-
-
-
-
-
-
+    
+for r in range(stroka):
+        for c in range(stolbec):
+            if matrix[r][c] == '*':
+                continue
+    
+        count=0
+        for dr in [-1, 0, 1]:
+            for dc in [-1, 0, 1]:
+                if 0 <= r +dr < stroka and 0 <= c + dc < stolbec:
+                    if matrix[r+dr][c+dc] == '*':
+                        count +=1                    
+        matrix[r][c]= count
 
 for row in matrix:
     print (' '.join(map(str, row)))
