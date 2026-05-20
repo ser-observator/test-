ed7bff46473ce4414e15f635a61813fef85ff99b
qwehgci-creator <276070954+qwehgci-creator@users.noreply.github.com>
2026-05-20 13:39:51 +0500

    )

--- yandex.py
+++ yandex.py
@@ -6,11 +6,18 @@
 #    print ('YES')
 #else: print ('no')
 stroka, stolbec, mina = map(int, input().split())
+
 matrix = [[0] *stolbec for _ in range(stroka)]
 
+for _ in range(mina):
+    r,c= map(int, input().split())
+    matrix[r-1][c-1]='*'
 
 
 
 
-#for row in matrix:
-#    print (' '.join(map(str, row)))
+
+
+
+for row in matrix:
+    print (' '.join(map(str, row)))
