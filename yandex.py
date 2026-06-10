39b02a2636e1eafe3f34f98a4cf53fa28e66a4f4
qwehgci-creator <276070954+qwehgci-creator@users.noreply.github.com>
2026-06-11 00:48:58 +0500

    сука

--- yandex.py
+++ yandex.py
@@ -37,3 +37,55 @@
 
 
 
+#def search(N):
+#    R=(bin(N))[2:]
+#    suma=0
+#    for _ in R:
+#        suma+=int (_)
+#    if suma % 2==0:
+#        New_R= '11'+R
+#    else:
+#        New_R= R+ '00'
+#    r = int(New_R, 2)
+#    return r
+#for N in range(1,100):
+#    r=search(N)
+#    if r>116:
+#        print(f'N={N}')
+#        break
+
+#def suma(N):
+#    a=N//100
+#    b= (N//10)%10
+#    c=N%10
+#    sum1= a+b
+#    sum2= b+c
+#    if 1<= sum1 <= 9 and 1<=sum2<=9:
+#        r=str(sum1)+str(sum2)
+#        return int(r)
+#    return None 
+#for N in range(100,999+1):
+#    result= suma(N)
+#    if result is not None:
+#        print (f'N={N}, Результат ={result}')
+#        break
+#
+#N=23
+#N=(bin(N))[2:]
+#suma=0
+#for _ in N:
+#    suma+=int (_)
+#if suma % 2 ==0:
+#    New_N= '10'+N
+#else:
+#    New_N = '1'+N
+#n= int(New_N,2)
+#print(n)
+
+
+
+
+
+
+#https://education.yandex.ru/ege/inf/task/998fbc59-2d80-4b1c-9ac4-6b6386b090ed
+#ебись нахуй
