727dfb4a1fdeebfea050bcdb293fa806a614b5d4
qwehgci-creator <276070954+qwehgci-creator@users.noreply.github.com>
2026-08-03 21:17:54 +0500

    sdfasdf

--- Z9/zadani9.py
+++ Z9/zadani9.py
@@ -1,8 +1,11 @@
 count = 0
-for line in open("input.txt"):
+for line in open('/home/obs/doc/python_and_informatika/test-/Z9/zadani9.txt'):
     a = [int(x) for x in line.split()]
-    p1= (a[0]+a[1])
-    p2= (a[2]+a[3])
-    if p1 == p2:
-        count += 1
-    l = max(a) < 
+    S= sum(a)
+    C1 = S % 2 == 0 and any(a[i] + a[j] == S //2 
+                                for i in range(4) for j in range (i+1, 4))
+    C2 = max(a) < sum(a) - max(a)
+    C3 = sum(a) % 2 == 0
+    if C1 and C2 and C3:
+        count +=1
+print (count)
