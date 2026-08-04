e467f457fede36eb9541519545e35490ea2f13b1
qwehgci-creator <276070954+qwehgci-creator@users.noreply.github.com>
2026-08-04 16:47:32 +0500

    я понял

--- informatica/Zadanie2/Z2.py
+++ informatica/Zadanie2/Z2.py
@@ -7,6 +7,7 @@
 #                F=int(((a and b)<= c) and ((b and c)<= d))
 #                if F == 0:
 #                    print (a, b, c, d, F)
+#решено dbac
 
 #https://education.yandex.ru/ege/inf/task/4c7c38c7-48e1-4428-af23-8ebbf2fbd088
 #from itertools import product
@@ -15,4 +16,12 @@
 #    F=int(((x and y) <= (not z))and (x <= y)or w)
 #    if F==0:
 #        print (x, y, z, w, F)
+#решено xzwy
 
+#https://education.yandex.ru/ege/inf/task/a0a0262b-ffa0-4fdb-a501-894d30e26aa9
+#from itertools import product
+#print ('x y w z F')
+#for x,y,w,z in product(range(0,2), repeat=4):
+#    F=int(((1==w)==(not((w and x)or y))) <= z)
+#    print (x, y, w, z, F)
+#решено xzyw
