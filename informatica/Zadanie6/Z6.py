f982aaeb42e958b08a75675c43b697a2d199ac9e
qwehgci-creator <276070954+qwehgci-creator@users.noreply.github.com>
2026-08-07 00:04:35 +0500

    qeqweqwe

--- informatica/Zadanie6/Z6.py
+++ informatica/Zadanie6/Z6.py
@@ -1,16 +1,12 @@
 from turtle import *
-speed(5)
-screensize(1500, 1500)
+speed(1)
+screensize(150, 150)
 
 k=10
 lt(90)
 
 
-for _ in range(2):
-    fd(8 * k)
-    rt(90)
-    fd(18 * k)
-    rt(90)
-    
+for _ in range(9):
+
     
 exitonclick()
