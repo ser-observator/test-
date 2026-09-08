2475c21191165fcefd5c16d8e2ac7a2398d2e7e5
qwehgci-creator <276070954+qwehgci-creator@users.noreply.github.com>
2026-09-08 20:44:57 +0500

    sdfasdfasf

--- informatica/Zadanie8/Z8.py
+++ informatica/Zadanie8/Z8.py
@@ -15,11 +15,45 @@
         # break
 
 #https://youtu.be/BudEmjf_RvM?si=7s5ozK-NrMLMGcgy
-from itertools import product
+# from itertools import product
 
+# k=0
+# for i in product('ЭТАН', repeat=5):
+#     slovo = ''.join(i)
+#     if (slovo.count('Э') + slovo.count('А')) == 1:
+#         k += 1
+# print (k)
+
+# from itertools import product
+# k=0
+
+# for i in product('0123456', repeat=5):
+#     number= ''.join(i)
+#     if number[0] != '0':
+#         if number.count('6') == 1:
+#             if number[0] != number[1] != number[2] != number[3] != number[4]:
+#                 k += 1
+# print (k)
+
+# from itertools import product
+# k=0
+# for i in product('0123456789ab', repeat=5):
+#     number= ''.join(i)
+#     if number[0] != '0':
+#         if number.count('7') == 1:
+#             if (number.count('9') + number.count('a') + number.count('b')) <=3:
+#                 k += 1
+# print (k)
+
+
+from itertools import product
 k=0
-for i in product('ЭТАН', repeat=5):
-    slovo = ''.join(i)
-    if (slovo.count('Э') + slovo.count('А')) == 1:
-        k += 1
+for i in product('0123456789', repeat=4):
+    number = ''.join(i)
+    if number[0] != '0':
+        if len(set(number)) == len(number):
+            number = number.replace('2', '0').replace('4', '0').replace('6', '0').replace('8', '0')
+            number = number.replace('3', '1').replace('5', '1').replace('7', '1').replace('9', '1')
+            if number.count ('00') == 0 and number.count('11')==0:
+                k += 1
 print (k)
