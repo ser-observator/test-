2fd0a5a1f301103a2d5df1fb9f52e370e978bff0
qwehgci-creator <276070954+qwehgci-creator@users.noreply.github.com>
2026-09-08 21:02:23 +0500

    фывафыва

--- informatica/Zadanie8/Z8.py
+++ informatica/Zadanie8/Z8.py
@@ -56,11 +56,19 @@
 
 # print (k)
 
+# from itertools import product
+# number=0
+# for i in product(sorted('БАТЫР'), repeat=5):
+#     slovo = ''.join(i)
+#     number +=1
+#     if slovo.count('Ы') == 0 and slovo.count('АА') == 0:
+#         print(number)
+#         break
+
 from itertools import product
 number=0
-for i in product(sorted('БАТЫР'), repeat=5):
+for i in product(sorted('ЛАЙМ'), repeat=5):
     slovo = ''.join(i)
     number +=1
-    if slovo.count('Ы') == 0 and slovo.count('АА') == 0:
+    if slovo.count('М') <=1 and slovo.count('ЛЛ')==0:
         print(number)
-        break
